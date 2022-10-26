76740eb9cbd8237d60e866dd7590eb19f94beab8
unknown <706710546@qq.com>
2022-10-26 19:36:59 +0800

    first commit

---
+++ 02_Get_mothed.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/get", getMsg)
+	//r.Run("127.0.0.1:9090")
+	r.Run(":9090")
+}
+
+func getMsg(c *gin.Context) {
+	name := c.Query("name")
+	c.String(http.StatusOK, "欢迎您:%s", name)
+
+}
